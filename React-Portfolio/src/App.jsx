46eb5b7cf75fda167ecc12525c11cac51d2449c0
C Grogan <conorgrogan23@gmail.com>
2024-03-08 02:05:46 +0000

    added functionality and responsiveness to nav section

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar } from './Components/Navbar/Navbar';
 function App() {
   return <div className={styles.App}>
     <Navbar />
+    <Hero />
   </div>;
 }
 
--- React-Portfolio/src/Components/Navbar/Navbar.jsx
+++ React-Portfolio/src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,20 @@ import styles from './Navbar.module.css';
 import { getImageURL } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return ( 
   <nav className={styles.navbar}>
     <a className={styles.title} href='/'>Portfolio</a>
     <div className={styles.menu}>
-      <img className={styles.menuBtn} src={getImageURL("nav/menuIcon.png")} alt='menu button'/>
-      <ul className={styles.menuItems}>
+      <img className={styles.menuBtn} src={
+        menuOpen
+        ? getImageURL("nav/closeIcon.png")
+        : getImageURL("nav/menuIcon.png")
+      } 
+      alt='menu button'
+      onClick={() => setMenuOpen(!menuOpen)}
+      />
+      <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen (false)}>
         <li>
           <a href='#about'>About</a></li>
           <li><a href='#experience'>Skills</a></li>
